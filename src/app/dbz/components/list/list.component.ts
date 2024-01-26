98abbe4b7984c2aefc268572b019e66e51f2db7e
Carlos1611 <erasmomartinez1611@gmail.com>
2024-01-25 23:10:24 -0300

    subir el resto del curso sección 5

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  standalone: false,
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+
+export class ListComponent {
+
+  @Input()
+  public characterList: Character[] = [{
+    name: ' Trunks',
+    power: 10
+  }]
+
+
+  //onDelete = Index value: number
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string):void {
+    if( !id ) return; // si no tiene id no retorna nada
+    this.onDelete.emit( id );
+  }
+
+}
